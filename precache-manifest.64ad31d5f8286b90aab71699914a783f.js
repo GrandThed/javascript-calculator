7d438babbf7b231a4bb8a0b8c0eed8dbe5144f90
GranThed <benjamincanas1@gmail.com>
2020-08-13 18:51:12 -0300

    Updates

---
+++ precache-manifest.64ad31d5f8286b90aab71699914a783f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "086e4462c23c26b61852e4a3a9dec6a0",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "824bf142dc2cf3067823",
+    "url": "/javascript-calculator/static/css/main.4f5461a8.chunk.css"
+  },
+  {
+    "revision": "17729a2f8e35c208da8d",
+    "url": "/javascript-calculator/static/js/2.e76dff30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/javascript-calculator/static/js/2.e76dff30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "824bf142dc2cf3067823",
+    "url": "/javascript-calculator/static/js/main.4ad0abb1.chunk.js"
+  },
+  {
+    "revision": "2a1d30e85db36d30dcbf",
+    "url": "/javascript-calculator/static/js/runtime-main.f3cb5538.js"
+  }
+]);
